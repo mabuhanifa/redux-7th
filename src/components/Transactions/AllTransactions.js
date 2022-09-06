1b4413babf3bb3fd91e62cbf5c8f735e98b5ae7e
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-06 23:17:59 +0600

    updated

--- src/components/Transactions/AllTransactions.js
+++ src/components/Transactions/AllTransactions.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const AllTransactions = () => {
-    return (
-        <div>
-            hello transaction
-        </div>
-    );
+  return (
+    <div>
+      <div>transactions</div>
+    </div>
+  );
 };
 
 export default AllTransactions;
